5d1b248887874b542dc2c5999cdcf423d90a4771
Dylan.Tsai <Dylan.tsai@bahwancybertek.com>
2020-06-01 10:25:40 +0800

    Upload version 1 SDK

---
+++ ContactTracerSDK.framework/Headers/ContactTracerSDK.h
@@ -0,0 +1,19 @@
+//
+//  ctsdk.h
+//  ctsdk
+//
+//  Created by Dylan Tsai on 2020/5/27.
+//  Copyright © 2020 Bahwancybertek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ctsdk.
+FOUNDATION_EXPORT double ctsdkVersionNumber;
+
+//! Project version string for ctsdk.
+FOUNDATION_EXPORT const unsigned char ctsdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ctsdk/PublicHeader.h>
+
+
